0945590265302f50c259770fa573135f3628aa00
jian.cui <jian.cui@kingsgroupgames.com>
2021-09-14 20:18:28 +0800

    更新材质球工具

--- Assets/Editor/Shader Control Center.cs
+++ Assets/Editor/Shader Control Center.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEditor;
+using UnityEditor.Sprites;
 using UnityEngine;
 
 
@@ -43,7 +44,7 @@ public class ShaderControlCenter : EditorWindow
     
     //%f代表ctrl+f快捷键 &代表alt  #代表shift
     [MenuItem("Assets/Check the material")]
-    static void TestMenu11()
+    static void CheckTheMaterial()
     {
         Debug.Log("查看材质球");
         UnityEngine.Object[] files=Selection.GetFiltered(typeof(UnityEngine.Object), SelectionMode.DeepAssets);
@@ -68,10 +69,132 @@ public class ShaderControlCenter : EditorWindow
         File.WriteAllText(Application.dataPath +  "/ShaderKey.json", json);
     }
     
+    [MenuItem("Assets/Generate The Atlas",true)]
+    static bool CheckType()
+    {
+        if(Selection.activeObject)
+        {
+            return Selection.activeObject.GetType() == typeof(Sprite);
+        }
+        return false;
+    }
+    [MenuItem("Assets/Generate The Atlas")]
+    static void GenerateTheAtlas()
+    {
+        Sprite sprite = Selection.activeObject as Sprite;
+        if(!sprite)
+        {
+            return;
+        }
+        
+        string atlasName;
+        Texture2D texture;
+        Packer.GetAtlasDataForSprite(sprite,out atlasName,out texture);
+        
+        Texture2D png = DuplicateTexture(texture);
+        string path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory) + "/" + atlasName + ".png";
+        Debug.Log("图片生成路径：" + path);
+        
+        byte[] dataBytes = png.EncodeToPNG();
+        
+        // 将字节保存成图片，这个路径只能在PC端对图片进行读写操作
+        File.WriteAllBytes(path, dataBytes);
+    }
+    
+    private static Texture2D DuplicateTexture(Texture2D source) {
+        RenderTexture renderTex = RenderTexture.GetTemporary(
+            source.width,
+            source.height,
+            0,
+            RenderTextureFormat.Default,
+            RenderTextureReadWrite.Linear);
+ 
+        Graphics.Blit(source, renderTex);
+        RenderTexture previous = RenderTexture.active;
+        RenderTexture.active = renderTex;
+        Texture2D readableText = new Texture2D(source.width, source.height);
+        readableText.ReadPixels(new Rect(0, 0, renderTex.width, renderTex.height), 0, 0);
+        readableText.Apply();
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(renderTex);
+        return readableText;
+    }
+
     static string DictionaryToJson(Dictionary<string,string> dict)
     {
         var entries = dict.Select(d =>
             string.Format("\"{0}\": [{1}]", d.Key, string.Join(",", d.Value)));
         return "{" + string.Join(",\n", entries) + "}";
+    }
+    
+    
+    static Dictionary<string, int> keywordRenderQueueModifier = new Dictionary<string, int>();
+    static Dictionary<int, string> QueueMatDic = new Dictionary<int, string>();
+    [MenuItem("Assets/Sort Mat By Keywords")]
+    static void OutPutMaterial()
+    {
+        ResetMaterial();
+        UnityEngine.Object[] MatAry=Selection.GetFiltered(typeof(UnityEngine.Material), SelectionMode.DeepAssets);
+
+        // var ary = MatAry.OrderBy(item =>
+        // {
+        //     return item.name.Contains("destroyed");
+        // }).ToList();
+        
+        foreach (var mat in MatAry)
+        {
+            Material newMat = mat as Material;
+            if (newMat.shaderKeywords.Length > 0)
+            {
+                var keywords = newMat.shader.name + ":";
+ 
+                foreach (var s in newMat.shaderKeywords)
+                    keywords += s + ",";
+ 
+                //Debug.Log(string.Format($"{newMat.shader.name} has additional keywords: {keywords}"));
+                //Debug.Log(keywords);
+
+                if (!keywordRenderQueueModifier.ContainsKey(keywords))
+                {
+                    keywordRenderQueueModifier.Add(keywords, keywordRenderQueueModifier.Count + 1);
+                }
+ 
+                //Debug.Log(string.Format($"keywordRenderQueueModifier[{newMat.shader.name}][{keywords}]={keywordRenderQueueModifier[keywords]}"));//[newMat.shader.name]
+                if (newMat.name.Contains("destroyed"))
+                {
+                    newMat.renderQueue += keywordRenderQueueModifier[keywords] + 100;
+                }
+                else
+                {
+                    newMat.renderQueue += keywordRenderQueueModifier[keywords];
+                }
+                
+                if (QueueMatDic.ContainsKey(newMat.renderQueue))
+                {
+                    QueueMatDic[newMat.renderQueue] = QueueMatDic[newMat.renderQueue] + "," + newMat.name;
+                }
+                else
+                {
+                    QueueMatDic.Add(newMat.renderQueue,newMat.name);
+                }
+            }
+        }
+
+        foreach (var QueueMat in QueueMatDic)
+        {
+            Debug.Log(QueueMat.Key + ":" + QueueMat.Value);
+        }
+    }
+
+    [MenuItem("Assets/Reset Material RenderQueue")]
+    static void ResetMaterial()
+    {
+        UnityEngine.Object[] MatAry=Selection.GetFiltered(typeof(UnityEngine.Material), SelectionMode.DeepAssets);
+        
+        foreach (var mat in MatAry)
+        {
+            Material newMat = mat as Material;
+            newMat.renderQueue = newMat.shader.renderQueue;
+        }
     }
 }
